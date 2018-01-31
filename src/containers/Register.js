b144e3a10aae3687c9caf2bd208130d85c804094
Reza Zeiny <rezazeiny1998@gmail.com>
2018-01-31 10:35:26 +0330

    some junks added

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-
-class Register extends Component {
+import "../styles/Register.css"
+class Input extends Component{
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+            <div className="Input">
+                <input autoComplete="false" required type={this.props.type} placeholder={this.props.placeholder} />
+                <label>{this.props.name}</label>
+            </div>
+        );
+    }
+}
+
+
+
+class Register extends Component {
+
+
+    render() {
+        return (
+            <div className="Register">
+                <header className="Register-header">
+                    <h1 className="Register-title">Register Page</h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <div className="Modal">
+                    <form onSubmit={this.props.onSubmit} className="ModalForm">
+                        <Input name={"username"} id="name" type="text" placeholder="Jack-Edward Oliver" />
+                        <Input name={"E-mail"} id="username" type="email" placeholder="mrjackolai@gmail.com" />
+                        <Input name={"password"} id="password" type="password" placeholder="password" />
+                        {/*<button>Log in <i className="fa fa-fw fa-chevron-right"></i></button>*/}
+                        <button>Log in</button>
+                    </form>
+                </div>
             </div>
         );
     }
